e384350d8dcc31e98b884918a335023fba0f2794
wk6577 <dsdkakjlasdf@naver.com>
2025-06-04 17:37:20 +0900

    스프링 베이스

---
+++ src/main/java/com/JobAyong/JobAyongApplication.java
@@ -0,0 +1,13 @@
+package com.JobAyong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAyongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAyongApplication.class, args);
+	}
+
+}
