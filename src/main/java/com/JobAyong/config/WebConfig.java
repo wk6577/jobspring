9c386c8426ea47d7c93626a131c171d547bd1c10
wk6577 <dsdkakjlasdf@naver.com>
2025-07-04 17:39:49 +0900

    스프링 자동화 제발 - 상훈

--- src/main/java/com/JobAyong/config/WebConfig.java
+++ src/main/java/com/JobAyong/config/WebConfig.java
@@ -7,15 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(true);
-    }
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         String os = System.getProperty("os.name").toLowerCase();
